79bbf4f7147dd29eeefbf2fac78f20bdedfadfd8
unknown <nicolasgonzalezmoller@gmail.com>
2024-03-14 22:37:34 -0300

    card

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const page = () => {
     return (
         <div>
-            proyectos
+
+            <div className='text-center text-5xl mt-7'>
+                Proyectos
+            </div>
+            <div className="max-w-xs mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-10">
+                <img className="w-full h-48 object-cover" src="https://via.placeholder.com/350" alt="Technology" />
+                <div className="py-4 px-6">
+                    <h2 className="text-lg font-semibold text-gray-800">Título</h2>
+                    <p className="mt-2 text-sm text-gray-600">Descripción</p>
+                    <p className="mt-2 text-xs text-gray-500">Tecnologías ocupadas:</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import Link from 'next/link'
+'use client'
+
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="bg-gray-800 py-4">
             <div className="max-w-7xl mx-auto px-4">
@@ -9,17 +17,29 @@ const Navbar = () => {
 
                     <div className="text-white text-lg font-semibold">Mi Navbar</div>
 
-                    <ul className="flex space-x-4">
-                        <li><Link href="/" className="text-white">Inicio</Link></li>
+                    <div className="lg:hidden">
+                        <button onClick={toggleMenu} className="text-white focus:outline-none transition-all duration-500 ease-in-out">
+                            <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                {isOpen ? (
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M4 6h16a1 1 0 010 2H4a1 1 0 110-2zm16 5H4a1 1 0 100 2h16a1 1 0 100-2zm0 5H4a1 1 0 100 2h16a1 1 0 100-2z" />
+                                ) : (
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M4 6h16a1 1 0 110 2H4a1 1 0 110-2zm16 5H4a1 1 0 010-2h16a1 1 0 010 2zm0 5H4a1 1 0 010-2h16a1 1 0 010 2z" />
+                                )}
+                            </svg>
+                        </button>
+                    </div>
+
+                    <ul className={`lg:flex lg:items-right transition-all duration-500 ease-in-out ${isOpen ? 'block' : 'hidden'} space-x-4 mt-4 lg:mt-0`}>
+                        <li><Link href="/" className="text-white ml-4">Inicio</Link></li>
                         <li><Link href="/proyectos" className="text-white">Proyectos</Link></li>
                         <li><Link href="/contacto" className="text-white">Contacto</Link></li>
-                        <li><a href="#" className="text-white">Colaboremos</a></li>
+                        <li><Link href="#" className="text-white">Colaboremos</Link></li>
                     </ul>
+
                 </div>
             </div>
         </nav>
+    );
+};
 
-    )
-}
-
-export default Navbar
+export default Navbar;
