1acf892888f759720c98e68f2c7ae47d731f30fd
unknown <nicolasgonzalezmoller@gmail.com>
2024-03-21 22:44:44 -0300

    form act

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -10,55 +10,61 @@ const page = () => {
                 <h1 className='text-3xl'>Contáctame</h1>
                 <p className='text-xl mt-5'>Rellena el siguiente formulario y te contactaré lo antes posible</p>
             </div>
-            <div class="max-w-md mx-auto mt-10">
-                <form class="bg-amber-400 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="max-w-md mx-auto mt-10">
+
+
+                <form className="bg-amber-400 shadow-md rounded px-8 pt-6 pb-8 mb-4" action="https://formsubmit.co/nicolasgonzalezmoller@gmail.com" method="POST">
 
 
 
 
-                    <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
                             Nombre
                         </label>
                         <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="name"
-                            type="text"
+                            type="name"
                             placeholder="Nombre"
+                            name='name'
                         />
                     </div>
-                    <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
                             Correo Electrónico
                         </label>
                         <input
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="email"
                             type="email"
                             placeholder="Correo Electrónico"
+                            name='email'
                         />
                     </div>
-                    <div class="mb-6">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="message">
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" for="message">
                             Mensaje
                         </label>
                         <textarea
-                            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             id="message"
                             placeholder="Mensaje"
+                            type='text'
+                            name='text'
                         ></textarea>
                     </div>
-                    <div class="flex items-center justify-between">
+                    <div className="flex items-center justify-between">
                         <button
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="button"
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            type="submit"
                         >
                             Enviar
                         </button>
                     </div>
                 </form>
-                <div class="max-w-md mx-auto mt-10 flex justify-center">
-                    <div class="flex items-center space-x-4">
+                <div className="max-w-md mx-auto mt-10 flex justify-center">
+                    <div className="flex items-center space-x-4">
                         <a href="https://www.linkedin.com/in/nicol%C3%A1s-gonz%C3%A1lez-moller-099767174/" target="_blank">
                             <img src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="anirudh-rai-072732220" height="30" width="40" />
                         </a>
