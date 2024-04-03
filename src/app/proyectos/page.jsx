360d1347cfec0168dd6dda73c67b05526e8a4712
unknown <nicolasgonzalezmoller@gmail.com>
2024-04-02 21:48:01 -0300

    add

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+'use client'
 
+import React from 'react';
+import Link from 'next/link';
 const Page = () => {
+
+    
     const proyectos = [
         {
             nombre: "GameLink",
@@ -22,7 +26,7 @@ const Page = () => {
             nombre: "Todo TypeScript",
             img: "https://res.cloudinary.com/dpbpyzl96/image/upload/v1710980703/Portafolio/bcb2lyccjcjlealzrwoj.png",
             descripcion: "App Web para organizar tareas",
-            tecnologias: "React, TypeScript",
+            tecnologias: "React, TypeScript, PicoCss",
             url: "https://tsxtodo.netlify.app/"
         },
         {
@@ -36,15 +40,29 @@ const Page = () => {
             nombre: "Todo Frontend",
             img: "https://res.cloudinary.com/dpbpyzl96/image/upload/v1710982037/Portafolio/p4jsm5uxcqf1y8xu5ggr.png",
             descripcion: "App Web para organizar Panoramas",
-            tecnologias: "React, NodeJs, Material UI",
+            tecnologias: "React, Material UI, FrontendMentor",
             url: "https://frontmentorchallengetodo.netlify.app"
         },
         {
             nombre: "Ji Project",
             img: "https://res.cloudinary.com/dpbpyzl96/image/upload/v1710982344/Portafolio/fvsuxgyg5j8udntqlg9k.png",
             descripcion: "SPA",
-            tecnologias: "HTML, CSS, JAVASCRIPT",
+            tecnologias: "HTML, CSS, Bootstrap, JAVASCRIPT",
             url: "https://usuarioo1.github.io/jiproject/"
+        },
+        {
+            nombre: "Encriptador de Texto",
+            img: "https://res.cloudinary.com/dpbpyzl96/image/upload/v1711316236/Portafolio/vgep8ty6mgrplxsydw19.png",
+            descripcion: "App web encriptador de texto",
+            tecnologias: "HTML, CSS, JAVASCRIPT",
+            url: "https://usuarioo1.github.io/encriptador-texto/"
+        },
+        {
+            nombre: "Api Web",
+            img: "https://res.cloudinary.com/dpbpyzl96/image/upload/v1712102273/Portafolio/dkm9a1csbyafnqtj3uqw.png",
+            descripcion: "SPA sobre APIS",
+            tecnologias: "Next JS, Tailwind, DaisyUI",
+            url: "https://api-web-inky.vercel.app/"
         }
 
     ];
@@ -62,6 +80,11 @@ const Page = () => {
                             <h2 className="text-lg font-semibold text-gray-800">{proyecto.nombre}</h2>
                             <p className="mt-2 text-sm text-gray-600">{proyecto.descripcion}</p>
                             <p className="mt-2 text-xs text-gray-500">Tecnologías ocupadas: {proyecto.tecnologias}</p>
+                            <div className="rounded-xl mt-3 max-w-32 bg-transparent items-center justify-center flex border-2 border-sky-500 shadow-lg hover:bg-sky-500 text-sky-500 hover:text-white duration-300 cursor-pointer active:scale-[0.98] mx-auto">
+                                <Link href={proyecto.url}> <button className="px-5 py-2">Visitame</button></Link>
+                            </div>
+
+
                         </div>
                     </div>
                 ))}
